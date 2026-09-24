c96b09e320ee6c878624ea7c1e601aadd4da7c05
Refi Fajriyani <refifjrn14@gmail.com>
2026-09-25 02:49:27 +0700

    chore: remove patch scripts

--- patch_get2.php
+++
@@ -1,5 +0,0 @@
-<?php
-$file2 = "resources/views/exam-cards/preview.blade.php";
-$content2 = file_get_contents($file2);
-$content2 = str_replace('@csrf', '', $content2);
-file_put_contents($file2, $content2);
