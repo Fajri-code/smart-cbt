f7ab803afb64628f6beb178aa1c5ba6b88168bdd
Refi Fajriyani <refifjrn14@gmail.com>
2026-09-24 22:53:25 +0700

    chore: clean up temp scripts again

--- patch_route.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = 'routes/web.php';
-$content = file_get_contents($file);
-
-$search = "->name('kartu-ujian.preview');";
-$replace = "->name('kartu-ujian.preview');\n    Route::match(['get', 'post'], '/kartu-ujian/pengaturan', [App\Http\Controllers\ExamCardController::class, 'settings'])->name('kartu-ujian.settings');";
-$content = str_replace($search, $replace, $content);
-
-file_put_contents($file, $content);
