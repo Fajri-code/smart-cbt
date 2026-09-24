f5b5fac12b191ccfeec60797be21835124c4b283
Refi Fajriyani <refifjrn14@gmail.com>
2026-09-24 23:43:44 +0700

    chore: remove patch scripts

--- patch_bank_p.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = 'resources/views/guru/bank-soal/show.blade.php';
-$content = file_get_contents($file);
-
-$search = '<p><span class="font-semibold">{{ strtoupper($option) }}.</span> {!! $question->{\'opsi_\'.$option} !!}</p>';
-$replace = '<div><span class="font-semibold">{{ strtoupper($option) }}.</span> {!! $question->{\'opsi_\'.$option} !!}</div>';
-$content = str_replace($search, $replace, $content);
-
-file_put_contents($file, $content);
