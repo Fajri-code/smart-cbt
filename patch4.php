f13057868f24e133471ce72529b697ea156e8a1a
Refi Fajriyani <refifjrn14@gmail.com>
2026-09-24 22:39:24 +0700

    chore: clean up temp scripts again

--- patch4.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = 'app/Http/Controllers/ExportController.php';
-$content = file_get_contents($file);
-
-$search3 = "                'Tipe Soal' => \$ans->question->tipe === 'pilihan_ganda' || \$ans->question->tipe === 'pg' ? 'Pilihan Ganda' : 'Essay',";
-$replace3 = "                'Tipe Soal' => \$ans->question->tipe === 'pilihan_ganda' || \$ans->question->tipe === 'pg' ? 'Pilihan Ganda' : 'Essay',\n                'Pertanyaan' => strip_tags(\$ans->question->pertanyaan),";
-$content = str_replace($search3, $replace3, $content);
-
-file_put_contents($file, $content);
